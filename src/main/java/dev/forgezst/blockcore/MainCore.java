3426fbaeeb5ca4d41275dd44b6db9e26176532a8
xPyron <112647408+HAlfredoSH@users.noreply.github.com>
2023-08-12 02:44:33 -0600

    Update MainCore.java

--- src/main/java/dev/forgezst/blockcore/MainCore.java
+++ src/main/java/dev/forgezst/blockcore/MainCore.java
@@ -1,37 +1,48 @@
 package dev.forgezst.blockcore;
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public final class MainCore extends JavaPlugin implements Listener {
-
-    @Override
-    public void onEnable() {
-        System.out.println(ChatColor.translateAlternateColorCodes('&',"&d&lBC&8: &aInicio correctamente"));
-        Bukkit.getPluginManager().registerEvents(this, this);
-    }
-
-    @EventHandler
-    public void onBlockPlace(BlockPlaceEvent event) {
-        Block block = event.getBlock();
-        if (block.getType() == Material.COBWEB) {
-           new BukkitRunnable() {
-                @Override
-                public void run() {
-                    block.setType(Material.AIR);
-                }
-            }.runTaskLater(this, 30 * 10); // 60 segundos (20 ticks/segundo)
+  public void onEnable() {
+    System.out.println(ChatColor.translateAlternateColorCodes('&', "&e&lBC&8: &astart successfully"));
+    Bukkit.getPluginManager().registerEvents(this, (Plugin)this);
+  }
+  
+  @EventHandler
+  public void onBlockPlace(BlockPlaceEvent event) {
+    final Block block = event.getBlock();
+    final Player player = event.getPlayer();
+    if (block.getType() != Material.COBWEB) {
+      event.setCancelled(true);
+      return;
+    } 
+    (new BukkitRunnable() {
+        int timeLeft = 30;
+        
+        public void run() {
+          if (this.timeLeft <= 0) {
+            block.setType(Material.AIR);
+            cancel();
+            return;
+          } 
+          player.spigot().sendMessage(ChatMessageType.ACTION_BAR, 
+              TextComponent.fromLegacyText(ChatColor.YELLOW + "Blocks placed will be removed at: " + this.timeLeft + "s"));
+          this.timeLeft--;
         }
-    }
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
+      }).runTaskTimer((Plugin)this, 0L, 20L);
+  }
+  
+  public void onDisable() {}
 }
